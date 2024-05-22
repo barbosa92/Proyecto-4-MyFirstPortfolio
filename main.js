3e107ef5c66780cff9a92aad9d5bb0cc8815acb5
vimarupon@hotmail.com <vimarupon@gmail.com>
2024-05-22 12:13:30 +0200

    delete useless code

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 import { header } from "./src/components/Header/Header";
 import { AboutMe } from "./src/pages/AboutMe/AboutMe";
-import { Projects } from "./src/pages/Projects/Projects";
-import { Contact } from "./src/pages/Skills/Contact";
 import "./style.css";
 
 header();
-Contact();
-Projects();
 AboutMe();
-
-
-
